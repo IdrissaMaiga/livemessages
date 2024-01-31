4f7734580d43fbb9d6d603d67b8df2757fc85aa4
IdrissaMaiga <maigadrisking@gmail.com>
2024-01-31 14:43:44 +0100

    first test

---
+++ index.js
@@ -0,0 +1,40 @@
+const prt = process.env.PORT || 3000
+const express = require('express')
+const { google } = require('googleapis')
+const { sheets } = require('googleapis/build/src/apis/sheets')
+const app = express()
+app.get('/data', async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials_doc.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets'
+  })
+  const client = await auth.getClient()
+  const gsheets = google.sheets({ version: 'v4', auth: client })
+  const spreadsheetId = '1Gn0UsVq1r8nwn9V2iz6kkJd1TY-48rtZ3M-929Owvhs'
+  const metadata = await gsheets.spreadsheets.get({
+    spreadsheetId
+  })
+  const getrows = await gsheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: 'Feuille 1'
+  })
+  res.send(getrows.data)
+  /* const writterows = await gsheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: 'Feuille 1',
+    valueInputOption:"USER_ENTERED",
+    resource:
+    {
+       values:[]
+    }
+    
+    
+  }) */
+  res.send(getrows.data)
+})
+
+app.listen(prt, (req, res) => {
+  console.log(`unning on ${prt}"`)
+})
